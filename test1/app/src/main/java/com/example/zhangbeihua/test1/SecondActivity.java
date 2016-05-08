222e6672410f806d492a09b8e9e9e6177a4122f7
jeffbeihua <zhangbeihua@zhangbeihuas-MacBook.local>
2016-05-08 23:27:38 +1000

    datepick and timepick

--- test1/app/src/main/java/com/example/zhangbeihua/test1/SecondActivity.java
+++ test1/app/src/main/java/com/example/zhangbeihua/test1/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.zhangbeihua.test1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -10,12 +12,15 @@ import android.widget.Toast;
 /**
  * Created by zhangbeihua on 7/03/16.
  */
+
 public class SecondActivity extends Activity{
 
     private RatingBar rtbMyScore;
     private SeekBar skbChinaScore;
     private TextView tvChinaScore;
 
+    private DatePicker dpPicker;
+    private TimePicker tpPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,10 @@ public class SecondActivity extends Activity{
         rtbMyScore = (RatingBar) findViewById(R.id.rtbMyScore);
         skbChinaScore = (SeekBar) findViewById(R.id.skbChinaScore);
         tvChinaScore = (TextView) findViewById(R.id.tvChinaScore);
+
+        dpPicker = (DatePicker) findViewById(R.id.dpPicker);
+        tpPicker = (TimePicker) findViewById(R.id.tpPicker);
+
         rtbMyScore.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
@@ -35,7 +44,7 @@ public class SecondActivity extends Activity{
         skbChinaScore.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                tvChinaScore.setText("ZHONGHUAMINGMING"+progress);
+                tvChinaScore.setText("ZHONGHUAMINGMING" + progress);
             }
 
             @Override
@@ -48,6 +57,27 @@ public class SecondActivity extends Activity{
 
             }
         });
+
+        dpPicker.init(2014, 2, 15, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                Toast.makeText(SecondActivity.this, "shi:" + year + "-" + (month + 1) + "-" + day, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tpPicker.setIs24HourView(true);
+        tpPicker.setCurrentHour(20);
+        tpPicker.setCurrentMinute(20);
+        tpPicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int i, int i2) {
+                Toast.makeText(SecondActivity.this, "shi:" + i + "-" + i2, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
 
     }
 }
