2144161ce3a5981592e4d324c9c0bdb31abc5876
jeffbeihua <zhangbeihua@zhangbeihuas-MacBook.local>
2016-05-09 11:51:47 +1000

    18 save activity value

--- test1/app/src/main/java/com/example/zhangbeihua/test1/ActivityLifeCycle.java
+++ test1/app/src/main/java/com/example/zhangbeihua/test1/ActivityLifeCycle.java
@@ -3,13 +3,28 @@ package com.example.zhangbeihua.test1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 /**
  * Created by zhangbeihua on 9/05/16.
  */
 public class ActivityLifeCycle extends Activity{
+
+    private EditText etMessageBox;
+    private Button btnSendMessage;
+    private int count;
+
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifecycle);
+
+        if (savedInstanceState!=null){
+            count = savedInstanceState.getInt("count");
+            Toast.makeText(this,"count="+count,Toast.LENGTH_SHORT).show();
+        }
         Log.i("ActivityLifeCycle","onCreate done");
     }
     //自动载入   code to override method
@@ -28,6 +43,7 @@ public class ActivityLifeCycle extends Activity{
     @Override
     protected void onResume() {
         super.onResume();
+        count ++;
         Log.e("ActivityLifeCycle", "onResume done");
     }
 
@@ -47,5 +63,12 @@ public class ActivityLifeCycle extends Activity{
     protected void onDestroy() {
         super.onDestroy();
         Log.e("ActivityLifeCycle", "onDestory done");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("count",count);
     }
 }
