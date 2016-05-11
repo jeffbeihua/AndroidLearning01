7697bec9708074d6f710fe0efdbf0ad2b1e6b21a
jeffbeihua <zhangbeihua@zhangbeihuas-MacBook.local>
2016-05-11 11:45:47 +1000

    http first success

--- PatientCase/app/src/main/java/com/example/zhangbeihua/patientcase/Activity/CaseActivity.java
+++ PatientCase/app/src/main/java/com/example/zhangbeihua/patientcase/Activity/CaseActivity.java
@@ -1,26 +1,35 @@
 package com.example.zhangbeihua.patientcase.Activity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.example.zhangbeihua.patientcase.R;
 
-import java.net.HttpURLConnection;
-//import com.example.zhangbeihua.patientcase.widget.AlertDialogHelper;
+import android.os.AsyncTask;
 
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.example.zhangbeihua.patientcase.widget.HTTPMethods;
+import com.example.zhangbeihua.patientcase.widget.HTTPMethods.JSONTask;
+import com.example.zhangbeihua.patientcase.widget.prmja_com;
+import java.util.concurrent.ExecutionException;
 
 public class CaseActivity extends AppCompatActivity {
 
-    EditText etTag;
-    HttpURLConnection urlConnection = null;
+    private EditText etTag;
 
 
 
@@ -28,11 +37,67 @@ public class CaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case);
-
         etTag = (EditText)findViewById(R.id.etTag);
+
+        //HTTPMethods HTTP = new HTTPMethods();
+
+        new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
+
+        //Log.d("json", task.getResult());
+
+
+
+
+
+
 
     }
 
+    public class JSONTask extends AsyncTask<String, String, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try{
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine())!=null){
+                    buffer.append(line);
+                }
+                return buffer.toString();
 
 
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            } finally {
+                if (connection != null){
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null){
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //return result;
+            etTag.setText(result);
+        }
+    }
 }
---
+++ PatientCase/app/src/main/java/com/example/zhangbeihua/patientcase/widget/HTTPMethods.java
@@ -0,0 +1,80 @@
+package com.example.zhangbeihua.patientcase.widget;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by zhangbeihua on 12/05/16.
+ */
+public class HTTPMethods {
+    String httpResult = null;
+    public class JSONTask extends AsyncTask<String, String, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try{
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine())!=null){
+                    buffer.append(line);
+                }
+                return buffer.toString();
+
+
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            } finally {
+                if (connection != null){
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null){
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            httpResult = result;
+            //etTag.setText(result);
+        }
+    }
+
+
+    protected String getResult() {
+        return httpResult ;
+    }
+
+
+}
