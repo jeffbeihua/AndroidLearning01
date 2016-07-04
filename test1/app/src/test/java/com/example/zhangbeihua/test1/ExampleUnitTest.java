c170b98085edf1c8c422166582419991e9e559f1
jeffbeihua <zhangbeihua@zhangbeihuas-MacBook.local>
2016-07-05 09:57:11 +1000

    change test

--- test1/app/src/test/java/com/example/zhangbeihua/test1/ExampleUnitTest.java
+++ test1/app/src/test/java/com/example/zhangbeihua/test1/ExampleUnitTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ *
+ * 
+ * testestset
  */
 public class ExampleUnitTest {
     @Test
