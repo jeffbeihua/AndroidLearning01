08c2e61d626ed145cf51d88ee112ecb546a93860
jeffbeihua <zhangbeihua@zhangbeihuas-MacBook.local>
2016-07-04 10:39:20 +1000

    test

--- test1/app/src/androidTest/java/com/example/zhangbeihua/test1/ApplicationTest.java
+++ test1/app/src/androidTest/java/com/example/zhangbeihua/test1/ApplicationTest.java
@@ -5,6 +5,9 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ test
+ test
+ test
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
